aaaece180dbd265d658c22795e9a5f1629eb5364
ngamsom <setthasiri.ngm@gmail.com>
2022-01-24 15:21:48 +1100

    bring in story and fetch story when user on the dashboard

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const router = express.Router()
 const {ensureAuth, ensureGuest} = require('../middleware/auth')
 
+//bring in story
+const Story = require('../models/Story')
+
 //@des Login/landing page
 //@route GET /
 router.get('/', ensureGuest, (req,res) => {
@@ -10,10 +13,18 @@ router.get('/', ensureGuest, (req,res) => {
     })
 })
 
-router.get('/dashboard', ensureAuth, (req,res) => {
-    res.render('dashboard', {
-        name: req.user.firstName
-    })
+router.get('/dashboard', ensureAuth, async (req,res) => {
+    try {
+        const stories = await Story.find({user: req.user.id}).lean()
+        res.render('dashboard', {
+            name: req.user.firstName,
+            stories
+        })
+    } catch(err) {
+        console.error(err)
+        res.render('error/500')
+    }
+
 })
 
 module.exports = router
