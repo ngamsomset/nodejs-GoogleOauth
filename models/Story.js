7b7a87b6c215ef982911d2670809b88a359dd6ef
ngamsom <setthasiri.ngm@gmail.com>
2022-01-24 15:20:33 +1100

    add trim

--- models/Story.js
+++ models/Story.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 const StorySchema = new mongoose.Schema({
     title: {
         type: String,
-        required: true
+        required: true,
+        trim: true,
     },
     body: {
         type: String,
