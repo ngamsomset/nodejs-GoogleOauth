5ed50f30ccb5d02f84a524505a1832a9222af51d
ngamsom <setthasiri.ngm@gmail.com>
2022-01-25 13:14:06 +1100

    add route for add story

--- routes/stories.js
+++ routes/stories.js
@@ -11,4 +11,16 @@ router.get('/add', ensureAuth, (req, res) => {
     res.render('stories/add')
 })
 
+//@des Process add form
+//@route POST /stories/add
+router.post('/', ensureAuth, async (req, res) => {
+    try {
+        req.body.user = req.user.id
+        await Story.create(req.body)
+        res.redirect('/dashboard')
+    } catch (err) {
+        console.error(err)
+        res.render('error/500')
+    }
+})
 module.exports = router
